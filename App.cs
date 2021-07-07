6913871dc006901c5f224725a82df04307233a1e
Kovalenko, Johann (LGBS) <johann.kovalenko@dlh.de>
2021-07-07 13:16:50 +0200

    Commit 4

--- App.cs
+++ App.cs
@@ -7,5 +7,6 @@ class App
         Console.WriteLine("First Line");
         Console.WriteLine("Second Line");
         Console.WriteLine("Third Line");
+        Console.WriteLine("Fourth Line");
     }
 }
